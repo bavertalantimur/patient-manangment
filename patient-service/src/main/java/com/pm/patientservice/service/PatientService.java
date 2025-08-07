5987c02811e266c6cedcb5ae31359263ea185500
Baver Talantimur <bawer.talantimur@gmail.com>
2025-08-08 00:14:50 +0300

    .

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -4,6 +4,7 @@ import com.pm.patientservice.dto.PatientRequestDto;
 import com.pm.patientservice.dto.PatientResponseDto;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
+import com.pm.patientservice.kafka.KafkaProducer;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -19,6 +20,7 @@ public class PatientService {
 
     private final PatientRepository patientRepository;
     private final PatientMapper patientMapper;
+    private final KafkaProducer kafkaProducer;
 
 
     public List<PatientResponseDto> getAllPatients() {
@@ -34,6 +36,7 @@ public class PatientService {
             throw new EmailAlreadyExistsException("A patient with this email " +"already exists "+ patientRequestDto.getEmail());
         }
         Patient patient =    patientRepository.save(patientMapper.toEntity(patientRequestDto));
+        kafkaProducer.sendEvent(patient);
         return patientMapper.toDto(patient);
     }
 
